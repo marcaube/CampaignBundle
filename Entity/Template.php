0400e05220ae210d044bdda365d190712e9cfb82
marcaube <marc@tourismecie.com>
2014-01-09 09:58:44 -0500

    Create a template entity

---
+++ Entity/Template.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace CampaignBundle\Model;
+
+use Campaign\Model\Template as BaseTemplate;
+
+/**
+ * @Entity
+ * @Table(name="ob_templates")
+ */
+class Template extends BaseTemplate
+{
+    /**
+     * @Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @Column(type="text")
+     */
+    private $code;
+
+    /**
+     * @Column(type="string", length=255, nullable=true)
+     */
+    private $snapshot;
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $snapshot
+     */
+    public function setSnapshot($snapshot)
+    {
+        $this->snapshot = $snapshot;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSnapshot()
+    {
+        return $this->snapshot;
+    }
+}
