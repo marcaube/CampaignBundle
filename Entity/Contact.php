c6604dd9d3a9d20fe67fab158535c2680bf21bdd
marcaube <marc@tourismecie.com>
2014-01-09 09:49:11 -0500

    Create a contact entity

---
+++ Entity/Contact.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CampaignBundle\Model;
+
+use Campaign\Model\Contact as BaseContact;
+
+/**
+ * @Entity
+ * @Table(name="ob_contacts")
+ */
+class Contact extends BaseContact
+{
+    /**
+     * @Column(type="object")
+     */
+    private $email;
+
+    /**
+     * @Column(type="array")
+     */
+    private $columns;
+
+    /**
+     * @param Email $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return Email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param array $columns
+     */
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+    }
+
+    /**
+     * @return array
+     */
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+}
