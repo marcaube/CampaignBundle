437f0a9c9c0eeebfe5a9ff8e0613b6f9ee13bd45
marcaube <marc@tourismecie.com>
2014-01-15 10:12:45 -0500

    Move the snapshot util inside the bundle

---
+++ Utils/Snapshot.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ob\CampaignBundle\Utils;
+
+use Knp\Snappy\Image;
+
+class Snapshot
+{
+    /**
+     * @var string
+     */
+    private $folder;
+
+    /**
+     * @var string
+     */
+    private $binaryPath;
+
+    /**
+     * @param null   $folder     The folder where to save the image
+     * @param string $binaryPath The path to the wkhtmltoimage binary
+     */
+    public function __construct($folder = null, $binaryPath = '/usr/local/bin/wkhtmltoimage')
+    {
+        $this->folder = $folder;
+        $this->binaryPath = $binaryPath;
+    }
+
+    /**
+     * @param string $html
+     * @param null   $fileName
+     *
+     * @return string
+     */
+    public function render($html, $fileName = null)
+    {
+        if (null == $fileName) {
+            $fileName = uniqid() . '.jpg';
+        }
+
+        $filePath = $this->folder . $fileName;
+
+        $snappy = new Image($this->binaryPath);
+        $snappy->setTimeout(0);
+        $snappy->generateFromHtml($html, $filePath);
+
+        return $fileName;
+    }
+}
